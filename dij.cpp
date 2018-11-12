af5c1e91902a164d88b7d55d11b786215de038f9
hhoudini010 <42649616+hhoudini010@users.noreply.github.com>
2018-11-12 19:30:41 +0530

    Final update.

--- dij.cpp
+++ dij.cpp
@@ -314,27 +314,6 @@ int main(int argc, char const *argv[])
 	vector<pair<lli, lli> > adj[v] ;
 	
 
-	// addEdge(adj, 0, 1, 4); 
- //    addEdge(adj, 0, 7, 8); 
- //    addEdge(adj, 1, 2, 8); 
- //    addEdge(adj, 1, 7, 11); 
- //    addEdge(adj, 2, 3, 7); 
- //    addEdge(adj, 2, 8, 2); 
- //    addEdge(adj, 2, 5, 4); 
- //    addEdge(adj, 3, 4, 9); 
- //    addEdge(adj, 3, 5, 14); 
- //    addEdge(adj, 4, 5, 10); 
- //    addEdge(adj, 5, 6, 2); 
- //    addEdge(adj, 6, 7, 1); 
- //    addEdge(adj, 6, 8, 6); 
- //    addEdge(adj, 7, 8, 7); 
-
-
-    // addEdge(adj,0,1,5) ;
-    // addEdge(adj,1,2,6) ;
-    // addEdge(adj,2,3,2) ;
-    // addEdge(adj,0,2,15) ;
-
     cout<<"\nEnter the number of edges : " ;
     cin>>n_edges ;
 
